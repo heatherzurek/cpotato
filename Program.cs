1c73342288a5255fafde1bc1b9673972c32e376c
Heather Zurek <heather.zurek@gmail.com>
2019-08-18 16:48:07 -0700

    double decimal range and value

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ namespace myApp
             int a = 6;
             int b = 4;
             int c = 3;
-            int d = (a + b) / c;
+            int d = (a  + b) / c;
             int e = (a + b) % c;
             Console.WriteLine($"quotient: {d}");
             Console.WriteLine($"remainder: {e}");
@@ -56,11 +56,50 @@ namespace myApp
             Console.WriteLine($"An example of overflow: {what}");
         }
 
+        static void WorkWithDoubles()
+        {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a  + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e  + f) / g;
+            Console.WriteLine(h);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+
+            double i = ( e - g ) / c;
+            Console.WriteLine(i);
+        }
+
         static void Main(string[] args) 
         {
             // WorkingWithIntegers();
             // OrderPrecedence();
             // TestLimits();
+            WorkWithDoubles();
+
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine( c / d);
+
         }
     }
 }
