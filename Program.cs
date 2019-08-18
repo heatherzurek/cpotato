62b58b536986e2d57ddb9c158d686bc01c673898
Heather Zurek <heather.zurek@gmail.com>
2019-08-17 21:09:18 -0700

    order precedence

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace myApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WorkingWithIntegers()
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("The current time is " + DateTime.Now);
@@ -20,6 +20,31 @@ namespace myApp
             Console.WriteLine(c);
             c = a + b / b;
             Console.WriteLine(c);
+        }
+
+        static void OrderPrecedence()
+        {
+                        int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+            d = (a  + b) * c;
+            Console.WriteLine(d);
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e  + f) / g;
+            Console.WriteLine(h);
+        }
+
+        static void Main(string[] args) 
+        {
+            // WorkingWithIntegers();
+
+            // OrderPrecedence();
         }
     }
 }
