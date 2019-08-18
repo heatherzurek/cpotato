107294ef768c4fcb95970b2520668c430515220d
Heather Zurek <heather.zurek@gmail.com>
2019-08-17 20:47:21 -0700

    datetime/int prac

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+            int a = 28;
+            int b = 42;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+            c = a + b / b;
+            Console.WriteLine(c);
+        }
+    }
+}
