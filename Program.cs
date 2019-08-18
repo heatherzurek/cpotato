08bfa31fe62c4c4a1e128f28d8cc5426c63a948f
Heather Zurek <heather.zurek@gmail.com>
2019-08-18 09:56:27 -0700

    testing limits with maxvalue and minvalue

--- Program.cs
+++ Program.cs
@@ -40,11 +40,27 @@ namespace myApp
             Console.WriteLine(h);
         }
 
+        static void TestLimits()
+        {
+            int a = 6;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+        }
+
         static void Main(string[] args) 
         {
             // WorkingWithIntegers();
-
             // OrderPrecedence();
+            // TestLimits();
         }
     }
 }
